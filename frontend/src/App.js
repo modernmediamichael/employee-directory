9562772b0d70aeb2e0ba8daf8423407068a98370
Michael Muniz <michaelmuniz@outlook.com>
2020-11-30 15:28:11 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div>
+            This is my body content.
+        </div>
+    )
+}
+
+export default Body
